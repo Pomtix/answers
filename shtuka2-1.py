66b38e90d40e835c793b01b1e3ea8a7b7e2fd54c
Pomtix <136187246+Pomtix@users.noreply.github.com>
2023-06-21 21:02:22 +0400

    Create shtuka2-1.py

---
+++ shtuka2-1.py
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="shtuka2-2.css">
+    <meta charset="UTF-8">
+    <title>штука</title>
+</head>
+<body>
+    <h1>Заголовок</h1>
+    <h2>Заголовок</h2>
+    <h3>Заголовок</h3>
+    <h4>Заголовок</h4>
+    <h5>Заголовок</h5>
+    <h6>Заголовок</h6>
+<p class="paragraph-1">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+<p class="paragraph-2">
+    <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>
+</p>
+<p class="paragraph-3">
+    <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</i>
+</p>
+<ul>
+    <li>Первый пункт</li>
+    <li>Второй пункт</li>
+    <li>Третий пункт</li>
+</ul>
+<ol>
+    <li>Первый пункт</li>
+    <li>Второй пункт</li>
+    <li>Третий пункт</li>
+</ol>
+<div>
+    <img class="picture" src="https://i.pinimg.com/originals/23/3e/ea/233eeab3cc20e2310cd4d8cc1d244564.jpg" width="600" height="360"/>
+</div>
+<p class="paragraph-4">
+    <i>Mountains and a lake, with a small photo of a mountain and a creeper looking at the viewer through a window.</i>
+</p>
+</body>
+</html>
