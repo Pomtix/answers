28814326d27a8e1c8173505b7f6af591b0acecf6
Motpix <bossmolokosos3155@gmail.com>
2023-06-21 19:57:41 +0400

    first commit

--- shtuka1.py
+++ shtuka1.py
@@ -1,5 +1,6 @@
-def palindrome(string):
-    return string == string[::-1]
+def palindrome(string):         # функция palindrome принимает аргумент-строку
+    return string == string[::-1]       # сравнение строки с её перевернутым вариантом, возвращение True или False
 
 print(palindrome('level'))
 print(palindrome('dot'))
+
